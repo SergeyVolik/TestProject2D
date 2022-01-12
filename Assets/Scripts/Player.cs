b0ad0472cd5d052a4199772e08e100f91ef48982
SergeyVolik <volik.sergey112@gmail.com>
2022-01-12 21:18:42 +0200

    blood particle + bullet with trail

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestProject
+{
+    public class Player : MonoBehaviour
+    {
+        private int m_Health = 5;
+
+    }
+
+}
